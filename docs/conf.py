4337f99d48cb1e3dada1323b9ccd3a3f0a41def1
Michael Fladischer <FladischerMichael@fladi.at>
2016-02-02 10:08:51 +0100

    Bump version: 0.3.0 → 0.3.1

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ project = u'Django Accept Headers'
 year = '2015'
 author = u'Michael Fladischer'
 copyright = '{0}, {1}'.format(year, author)
-version = release = u'0.3.0'
+version = release = u'0.3.1'
 
 pygments_style = 'trac'
 templates_path = ['.']
